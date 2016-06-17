869e0bc946b91114126ca24ef6028e60547c8cac
amirali <yones.gholami45@gmail.com>
2016-06-18 03:20:20 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -241,7 +241,7 @@ function create_config( )
      "weather",
      "webshot",
      },
-    sudo_users = {180252366},
+    sudo_users = {158795145},
     admin_users = {},
     disabled_channels = {}
   }
